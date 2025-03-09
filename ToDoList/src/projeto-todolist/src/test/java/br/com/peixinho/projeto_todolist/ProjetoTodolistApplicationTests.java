d3cac8bdb14d3a4e2e77522ce90786d6b1a55547
Peixinho20 <ctlcamila45@gmail.com>
2025-03-09 17:09:02 -0300

    ...

---
+++ ToDoList/src/projeto-todolist/src/test/java/br/com/peixinho/projeto_todolist/ProjetoTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.peixinho.projeto_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
